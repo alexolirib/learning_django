eea07b944b4051d171868629f93bc81699fe1e6e
Alexandre Ribeiro <alexolirib@gmail.com>
2019-02-04 08:57:31 -0200

    fazendo algumas modificações na tela de admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
-
-# Register your models here.
 from polls.models import Question, Choice
 
-admin.site.register(Question)
+#para customizar a apresentação do admin
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+
+#uma forma
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+#outra forma
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+#outra forma em grupos
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'],
+         'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    #was_published_recently - método que o model possui
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
